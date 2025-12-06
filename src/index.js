e42b089002c0c02cc4c4944d7fe3d357677de8a7
ashwanikumar1809kr-cmd <ashwanikumar1809@gmail.com>
2025-12-07 00:14:21 +0530

    Tested userschema

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
+//const User = require('./schema/userSchema');
 
 const app = express();
 
@@ -18,9 +19,21 @@ app.post('/ping', (req, res) => {
 
 app.listen(ServerConfig.PORT,async () => {
     await connectDB();
-    console.log(`server started at port ${ServerConfig.PORT}...!!`);    
+    console.log(`server started at port ${ServerConfig.PORT}...!!`);
+    
+    //const newUser = await User.create({
+    //       email: 'a@b.com',
+    //       password: '123456',
+    //       firstName:'Jonathan',
+    //       lastName: 'Majors',
+    //       mobileNumber: '123123120'
+    // });
+
+    //console.log("Created new user");
+    //console.log(newUser);
+
 });
 
 
 //34.5.12.8:5500 -> socket  address for other laptop
-//localhost:5500-> socket address fpr own laptop       
+//localhost:5500-> socket address fpr own laptop        
